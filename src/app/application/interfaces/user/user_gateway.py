4d31ff2aeff4c5b2a5e3770b940350d7e1326b47
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-06-16 15:06:05 +0200

    Добавил интерактор аутентификации пользователя

--- src/app/application/interfaces/user/user_gateway.py
+++ src/app/application/interfaces/user/user_gateway.py
@@ -9,3 +9,11 @@ class UserSaver(Protocol):
     def save_user(self, user: User) -> None:
         """Сохраняет обьект пользователя."""
         ...
+
+
+class UserReader(Protocol):
+    """Интерфейс для чтения пользователя."""
+
+    def get_user_by_username(self, username: str) -> User:
+        """Получает пользователя по имени."""
+        ...
