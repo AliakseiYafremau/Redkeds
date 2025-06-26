d9601dfef44d04b219699f33295be293651e895b
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-06-26 16:48:42 +0200

    Добавил кэш для .venv

--- src/app/adapters/database.py
+++ src/app/adapters/database.py
@@ -9,9 +9,7 @@ def new_session_maker(
     database: str,
 ) -> async_sessionmaker[AsyncSession]:
     """Create a new async session maker for connecting to the PostgreSQL database."""
-    database_url = (
-        f"postgresql+psycopg://{login}:{password}@{host}:{port}/{database}"
-    )
+    database_url = f"postgresql+psycopg://{login}:{password}@{host}:{port}/{database}"
 
     engine = create_async_engine(
         database_url,
