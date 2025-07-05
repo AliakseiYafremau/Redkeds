e54c5c4d77a36b325520350c1973e74c7e01dbca
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-07-05 12:44:27 +0100

    Скорректировал работу alembic

--- src/app/adapters/migrations/env.py
+++ src/app/adapters/migrations/env.py
@@ -9,9 +9,6 @@ from app.adapters.database import build_database_url
 from app.adapters.models import Base
 from app.config import load_postgres_config
 
-config = context.config
-target_metadata = Base.metadata
-
 
 def get_database_url() -> str:
     """Возращает собранный postgres-url из переменных окружения."""
@@ -25,22 +22,37 @@ def get_database_url() -> str:
     )
 
 
+config = context.config
+target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", get_database_url())
+
+
 def run_migrations_offline() -> None:
-    """Запуск миграций в оффлайн-режиме."""
-    url = get_database_url()
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        render_as_batch=True,
     )
+
     with context.begin_transaction():
         context.run_migrations()
 
 
 def do_run_migrations(connection: Connection) -> None:
-    """Конфигурирует и запускает миграции."""
+    """Запускает миграцию."""
     context.configure(connection=connection, target_metadata=target_metadata)
 
     with context.begin_transaction():
@@ -48,24 +60,24 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_async_migrations() -> None:
-    """Асинхронный запуск миграций."""
-    url = get_database_url()
-    section = config.get_section(config.config_ini_section)
-    section["sqlalchemy.url"] = url
+    """In this scenario we need to create an Engine.
 
+    Associate a connection with the context.
+    """
     connectable = async_engine_from_config(
-        section,
+        config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
+
     await connectable.dispose()
 
 
 def run_migrations_online() -> None:
-    """Запуск миграций в онлайн-режиме (асинхронный)."""
+    """Run migrations in 'online' mode."""
     asyncio.run(run_async_migrations())
 
 
