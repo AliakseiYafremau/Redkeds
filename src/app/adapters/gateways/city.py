a455f90d8c1eb307082bf292be49112ad03beabd
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-07-14 21:45:23 +0100

    Добавил реализацию gateway для города

---
+++ src/app/adapters/gateways/city.py
@@ -0,0 +1,21 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.adapters.models import CityModel
+from app.application.interfaces.city.city_gateway import CityReader
+from app.domain.entities.city import City
+
+
+class CityGateway(
+    CityReader,
+):
+    """Gateway для работы с городами в базе данных."""
+
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def get_cities(self) -> list[City]:
+        """Получает информацию о всех городах."""
+        result = await self._session.execute(select(CityModel))
+        rows = result.fetchall()
+        return [City(id=row.id, name=row.name) for row in rows]
