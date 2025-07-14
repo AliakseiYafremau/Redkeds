a4115e3d1a3eea0dfb18b044c4a27a1808ddf689
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-07-15 00:33:05 +0100

    Добавил orm-модели

--- src/app/adapters/gateways/user.py
+++ src/app/adapters/gateways/user.py
@@ -62,3 +62,55 @@ class UserGateway(
 
     async def delete_user(self, user: UserId) -> None:
         """Удаляет пользователя."""
+
+class SQLUserGateway(
+    UserSaver,
+    UserUpdater,
+    UserReader,
+    UserDeleter,
+):
+    """Gateway для работы с данными пользователя."""
+
+    async def save_user(
+        self,
+        user: User,
+    ) -> None:
+        """Сохраняет пользователя в базе данных."""
+
+    async def get_user_by_id(self, user_id: UserId) -> User:
+        """Получает пользователя по id."""
+        return User(
+            id=user_id,
+            username="fake_username",
+            password="fake_password",
+            photo=None,
+            specialization=[],
+            city=CityId(uuid4()),
+            description="fake_description",
+            tags=[],
+            communication_method=CommunicationMethodId(uuid4()),
+            status=None,
+            showcase=None,
+        )
+
+    async def get_user_by_username(self, username: str) -> User:
+        """Получает пользователя по имени."""
+        return User(
+            id=UserId(uuid4()),
+            username=username,
+            password="fake_password",
+            photo=None,
+            specialization=[],
+            city=CityId(uuid4()),
+            description="fake_description",
+            tags=[],
+            communication_method=CommunicationMethodId(uuid4()),
+            status=None,
+            showcase=None,
+        )
+
+    async def update_user(self, user: User) -> None:
+        """Обновляет данные пользователя."""
+
+    async def delete_user(self, user: UserId) -> None:
+        """Удаляет пользователя."""
--- src/app/adapters/models.py
+++ src/app/adapters/models.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
 
 
 class Base(DeclarativeBase):
@@ -23,3 +24,64 @@ class CityModel(Base):
 
     id: Mapped[UUID] = mapped_column(primary_key=True)
     name: Mapped[str]
+
+
+class SpecializationModel(Base):
+    """Модель специальности."""
+
+    __tablename__ = "specializations"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    name: Mapped[str]
+
+
+class ShowcaseModel(Base):
+    """Модель витрины."""
+
+    __tablename__ = "showcases"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+
+
+class CommunicationMethodModel(Base):
+    """Модель предпочтения способа общения."""
+
+    __tablename__ = "communication_methods"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    name: Mapped[str]
+
+
+class UserModel(Base):
+    """Модель пользователя."""
+
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    password: Mapped[str]
+    photo: Mapped[str | None]
+    description: Mapped[str]
+    status: Mapped[str]
+
+    city_id: Mapped[UUID] = mapped_column(ForeignKey("cities.id"))
+    communication_method_id: Mapped[UUID] = mapped_column(ForeignKey("communication_methods.id"))
+    showcase_id: Mapped[UUID] = mapped_column(ForeignKey("showcases.id"))
+
+
+class UserTagModel(Base):
+    """Связь пользователей с их тегами."""
+
+    __tablename__ = "user_tag"
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
+    tag_id: Mapped[UUID] = mapped_column(ForeignKey("tags.id"))
+
+
+class UserSpecializationModel(Base):
+    """Связь пользователей с их специализациями."""
+
+    __tablename__ = "user_specialization"
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
+    specialization_id: Mapped[UUID] = mapped_column(ForeignKey("specializations.id"))
