3c7d424734cbbf0d5d75bb05ff8920f4f3324196
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-07-15 00:39:32 +0100

    Корректировки orm-моделей

--- src/app/adapters/models.py
+++ src/app/adapters/models.py
@@ -11,7 +11,7 @@ class Base(DeclarativeBase):
 class TagModel(Base):
     """Модель тега."""
 
-    __tablename__ = "tables"
+    __tablename__ = "tags"
 
     id: Mapped[UUID] = mapped_column(primary_key=True)
     name: Mapped[str]
@@ -60,9 +60,9 @@ class UserModel(Base):
     id: Mapped[UUID] = mapped_column(primary_key=True)
     username: Mapped[str]
     password: Mapped[str]
-    photo: Mapped[str | None]
+    photo: Mapped[str | None] = mapped_column(nullable=True)
     description: Mapped[str]
-    status: Mapped[str]
+    status: Mapped[str | None] = mapped_column(nullable=True)
 
     city_id: Mapped[UUID] = mapped_column(ForeignKey("cities.id"))
     communication_method_id: Mapped[UUID] = mapped_column(ForeignKey("communication_methods.id"))
@@ -74,8 +74,8 @@ class UserTagModel(Base):
 
     __tablename__ = "user_tag"
 
-    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
-    tag_id: Mapped[UUID] = mapped_column(ForeignKey("tags.id"))
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    tag_id: Mapped[UUID] = mapped_column(ForeignKey("tags.id"), primary_key=True)
 
 
 class UserSpecializationModel(Base):
@@ -83,5 +83,5 @@ class UserSpecializationModel(Base):
 
     __tablename__ = "user_specialization"
 
-    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
-    specialization_id: Mapped[UUID] = mapped_column(ForeignKey("specializations.id"))
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    specialization_id: Mapped[UUID] = mapped_column(ForeignKey("specializations.id"), primary_key=True)
