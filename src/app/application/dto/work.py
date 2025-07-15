7db310b8a1ad6cd356a853318f769dee0cb18fd6
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-07-15 21:04:52 +0100

    добавил dto

--- src/app/application/dto/work.py
+++ src/app/application/dto/work.py
@@ -1,10 +1,31 @@
 from dataclasses import dataclass
 
+from app.domain.entities.showcase import ShowcaseId
+
 
 @dataclass
 class NewWorkDTO:
     """DTO для создания новой работы."""
 
+    title: str
+    description: str
+    file_path: str
+
+
+@dataclass
+class UpdateWorkDTO:
+    """DTO для обновления работы."""
+
+    title: str
+    description: str
+    file_path: str
+
+
+@dataclass
+class ReadWorkDTO:
+    """DTO для чтения работы."""
+
+    showcase_id: ShowcaseId
     title: str
     description: str
     file_path: str
