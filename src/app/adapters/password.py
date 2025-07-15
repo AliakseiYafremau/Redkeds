9df1636a98927ad3a7c3c9c34a71fa87643e04a9
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-07-15 16:39:21 +0100

    Добавил хеширование пароля

--- src/app/adapters/password.py
+++ src/app/adapters/password.py
@@ -1,16 +1,19 @@
+import bcrypt
+
 from app.adapters.exceptions import InvalidPasswordError
 from app.application.interfaces.user.password_manager import PasswordHasher
 
 
-class FakePasswordHasher(PasswordHasher):
-    """Интерфейс для хеширования паролей."""
+class BcryptPasswordHasher(PasswordHasher):
+    """Реализация PasswordHasher с использованием bcrypt."""
 
     def hash_password(self, password: str) -> str:
         """Хеширует пароль."""
-        return password
+        hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+        return hashed.decode("utf-8")  # Преобразуем bytes -> str для хранения
 
     def verify_password(self, password: str, hashed_password: str) -> bool:
         """Проверяет пароль."""
-        if password == hashed_password:
+        if bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8")):
             return True
         raise InvalidPasswordError
