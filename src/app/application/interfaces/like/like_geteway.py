2661b022f720e1f408a9c1b897686175c2da9d6c
Ilya <barilya@yandex.ru>
2025-08-02 12:31:00 +0500

    Добавил для geteway лайков

---
+++ src/app/application/interfaces/like/like_geteway.py
@@ -0,0 +1,12 @@
+from typing import Protocol
+
+from app.domain.entities.user_id import UserId
+from app.domain.entities.showcase import ShowcaseId
+
+
+class AddLike(Protocol):
+    """Интерфейс для добавления лайков."""
+
+    async def add_like(self, user_id: UserId, showcase_id: ShowcaseId) -> bool:
+        """Добволяет лайк витрине от пользователя."""
+        ...
