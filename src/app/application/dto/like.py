fca6a05462d988a538a2f683bb20b7faa17f4ebb
Ilya <barilya@yandex.ru>
2025-08-04 12:53:23 +0500

    fixed naming and return type (None -> LikeId)

--- src/app/application/dto/like.py
+++ src/app/application/dto/like.py
@@ -5,7 +5,7 @@ from app.domain.entities.user_id import UserId
 
 
 @dataclass
-class LikeDTO:
+class NewLikeDTO:
     """DTO для добавления лайка."""
 
     user_id: UserId
--- src/app/application/interfaces/like/like_geteway.py
+++ src/app/application/interfaces/like/like_geteway.py
@@ -1,19 +1,26 @@
 from typing import Protocol
 
-from app.domain.entities.like import Like
+from app.domain.entities.like import Like, LikeId
 
 
-class AddLike(Protocol):
+class LikeSaver(Protocol):
     """Интерфейс для добавления лайков."""
 
-    async def add_like(self, like: Like) -> None:
+    async def add_like(self, like: Like) -> LikeId:
         """Добволяет лайк витрине от пользователя."""
         ...
 
 
-class DeleteLike(Protocol):
+class LikeDeleter(Protocol):
     """Интерфейс для удаления лайков."""
 
-    async def delete_like(self, like: Like) -> None:
+    async def delete_like(self, like_id: LikeId) -> None:
         """Удаляет лайк витрине от пользователя."""
+        ...
+
+class IdProvider(Protocol):
+    """Интерфейс для получения идентификатора."""
+
+    def __call__(self) -> LikeId:
+        """Возвращает ID лайка."""
         ...
