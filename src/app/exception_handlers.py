4ca3864939dccb49de4d11dc70c9f4937cb2f1cf
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-08-13 13:26:06 +0100

    Линтинг

--- src/app/exception_handlers.py
+++ src/app/exception_handlers.py
@@ -1,6 +1,6 @@
-from sqlalchemy.exc import IntegrityError
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from sqlalchemy.exc import IntegrityError
 
 from app.adapters.exceptions import (
     AuthenticationError,
