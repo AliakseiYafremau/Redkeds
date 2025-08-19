3111d018a9efb7e6cd99de530caedb3cd01ac0e7
Ilya <barilya@yandex.ru>
2025-08-19 23:40:16 +0500

    Fixed error in like and skip interactors

--- src/app/application/dto/skip.py
+++ src/app/application/dto/skip.py
@@ -1,12 +1,10 @@
 from dataclasses import dataclass
 
 from app.domain.entities.showcase import ShowcaseId
-from app.domain.entities.user_id import UserId
 
 
 @dataclass
 class NewSkipDTO:
     """DTO для добавления скипа."""
 
-    user_id: UserId
     showcase_id: ShowcaseId
--- src/app/application/interactors/like/add_like.py
+++ src/app/application/interactors/like/add_like.py
@@ -1,8 +1,9 @@
-from app.application.dto.like import NewLikeDTO
+from app.application.interfaces.common.id_provider import IdProvider
 from app.application.interfaces.common.transaction import TransactionManager
 from app.application.interfaces.common.uuid_generator import UUIDGenerator
 from app.application.interfaces.like.like_gateway import LikeSaver
 from app.domain.entities.like import Like, LikeId
+from app.domain.entities.showcase import ShowcaseId
 
 
 class AddLikeInteractor:
@@ -10,21 +11,24 @@ class AddLikeInteractor:
 
     def __init__(
         self,
+        id_provider: IdProvider,
         like_gateway: LikeSaver,
         transaction_manager: TransactionManager,
         uuid_generator: UUIDGenerator,
     ) -> None:
+        self._id_provider = id_provider
         self._like_gateway = like_gateway
         self._transaction_manager = transaction_manager
         self._uuid_generator = uuid_generator
 
-    async def __call__(self, data: NewLikeDTO) -> LikeId:
+    async def __call__(self, showcase_id: ShowcaseId) -> LikeId:
         """Добавляет лайк."""
+        user_id = self._id_provider()
         like_id = LikeId(self._uuid_generator())
         like = Like(
             id=like_id,
-            user_id=data.user_id,
-            showcase_id=data.showcase_id,
+            user_id=user_id,
+            showcase_id=showcase_id,
         )
         await self._like_gateway.add_like(like)
         await self._transaction_manager.commit()
