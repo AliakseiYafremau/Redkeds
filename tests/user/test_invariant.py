809992c43ec8e3c6b97b55516dfb47bd14be59cd
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-08-24 12:20:54 +0100

    Linting

--- tests/user/test_invariant.py
+++ tests/user/test_invariant.py
@@ -5,7 +5,9 @@ from tests.factories import make_user
 
 
 @pytest.mark.parametrize("nickname", ["", None])
-def test_creation_of_a_user_without_nickname_and_nickname_display(nickname: str | None) -> None:
+def test_creation_of_a_user_without_nickname_and_nickname_display(
+    nickname: str | None,
+) -> None:
     with pytest.raises(
         ValueError, match="Пользователь не может отображать nickname, если его нет."
     ):
